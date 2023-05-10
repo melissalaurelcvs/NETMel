dbde54cc113310b0933f62b43e328530cab0cae8
melissalaurelcvs <98424554+melissalaurelcvs@users.noreply.github.com>
2023-05-10 14:07:40 -0500

    Add files via upload

---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            var account = new BankAccount("Melissa", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+            // Test that the initial balances must be positive.
+            // BankAccount invalidAccount;
+            //try
+            //{
+            //    invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+
+            //}
+
+
+            // Test that the initial balances must be positive.
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+        }
+    }
+}
+
